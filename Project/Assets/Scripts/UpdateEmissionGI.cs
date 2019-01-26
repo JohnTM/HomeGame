efaebdc67bcdd8086b975540b2ded0c257730235
AIE\adam.bailey <adambailey@aie.edu.au>
2019-01-26 13:20:20 +1030

    Updated navmesh by adding invisible navmesh blockers.  Added unneccesary tv illumination effect.

---
+++ Project/Assets/Scripts/UpdateEmissionGI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class UpdateEmissionGI : MonoBehaviour {
+
+    Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RendererExtensions.UpdateGIMaterials(rend);
+	}
+}
