0065c3c0c8369cd4953fc91af8b6bbafb7d6b234
John Millard <john@twolivesleft.com>
2019-01-27 12:37:37 +1030

    Fixed Vacuum behaviour.

--- Project/Assets/Scripts/Vacuum.cs
+++ Project/Assets/Scripts/Vacuum.cs
@@ -11,16 +11,13 @@ public class Vacuum : MonoBehaviour {
     private AudioClip[] m_clips;
 
     private AudioSource m_audioSource1;
-    private AudioSource m_audioSource2;
 
     private Sequence m_turnOnSeq;
-    private Sequence m_turnOffSeq;
 
     // Use this for initialization
     void Start()
     {
         m_audioSource1 = gameObject.AddComponent<AudioSource>();
-        m_audioSource2 = gameObject.AddComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,10 +27,10 @@ public class Vacuum : MonoBehaviour {
 
     public void On()
     {
-        if (m_turnOffSeq != null)
+        if (m_turnOnSeq != null)
         {
-            m_turnOffSeq.Kill();
-            m_turnOffSeq = null;
+            m_turnOnSeq.Kill();
+            m_turnOnSeq = null;
         }
 
         m_body.DOShakePosition(1.0f, 0.1f, 20, 90, false, false).SetLoops(-1);
@@ -41,18 +38,11 @@ public class Vacuum : MonoBehaviour {
         m_turnOnSeq = DOTween.Sequence();
         m_turnOnSeq.AppendCallback(() =>
         {
-            m_audioSource1.volume = 1.0f;
+            m_audioSource1.volume = 0.0f;
             m_audioSource1.clip = m_clips[0];
             m_audioSource1.Play();
         });
-        m_turnOnSeq.AppendInterval(m_clips[0].length - 1.0f);
-        m_turnOnSeq.AppendCallback(() =>
-        {
-            m_audioSource2.clip = m_clips[1];
-            m_audioSource2.volume = 0.0f;
-            m_audioSource2.Play();
-            m_audioSource2.loop = true;
-        }).Append(m_audioSource2.DOFade(1.0f, 1.0f));
+        m_turnOnSeq.Append(m_audioSource1.DOFade(1.0f, 1.0f));
     }
 
     public void Off()
@@ -63,9 +53,10 @@ public class Vacuum : MonoBehaviour {
         {
             m_turnOnSeq.Kill();
             m_turnOnSeq = null;
-
-            m_turnOffSeq = DOTween.Sequence();
-            m_turnOnSeq.Append(m_audioSource2.DOFade(0.0f, 1.0f));
         }
+
+        m_turnOnSeq = DOTween.Sequence();
+        m_turnOnSeq.Append(m_audioSource1.DOFade(0.0f, 1.0f));
+
     }
 }
